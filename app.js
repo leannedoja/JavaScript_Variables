eb32ed6311583f8278acb1e3643ed3c216ef0d2c
leanne dozier <leannedoja@gmail.com>
2024-01-17 05:00:24 -0700

    completed lesson

--- app.js
+++ app.js
@@ -4,7 +4,47 @@ console.log(
 );
 
 // Exercise 1
+//const firstName;
+let lastName;
+var age;
 
 // Exercise 2
+firstName = "Leanne";
+lastName = "Dozier";
+age = 39;
+
+console.log(
+  `${firstName} ${lastName} was born in 1984, which makes her ${39} until later this year.`
+)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = "true";
+
+console.log(
+  `The year ${language} came out was ${createdYear}. ${isCaseSensitive} or false?`
+)
+
+//Exercise 4
+let item = "shoes";
+let price = 19.99;
+let isOnSale = "false";
+let salePercentage = 15;
+let stock = 0;
+let inStock = "false";
+let selectedSize = "M";
+
+console.log(
+  `The ${item} are ${salePercentage}% off of the original price of $${price}.`
+)
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = "true";
+
+console.log(
+  `The book called ${title}, written by ${author} has ${pageCount} pages, and the last page is bookmarked at page ${bookmark}.`
+)
